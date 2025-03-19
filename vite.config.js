17ebc8f80cd441551f781920c5a9cf1ec4981a52
ben sefton <benwpsefton@gmail.com>
2025-03-19 11:58:05 +0800

    updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,23 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({  
     plugins: [  
         tailwindcss(),  
-    ],  
+        usePHP({  
+            entry: [  
+                'index.php',  
+                'public/index.{html,php,js}',  
+                'App/views/**/*.{html,php,js}'  
+            ],  
+        }),  
+        liveReload([  
+                'index.php',  
+                'public/index.{html,php,js}',  
+                'App/views/**/*.{html,php,js}'  
+            ],  
+            {  
+                alwaysReload: true  
+            }  
+        ),  
+    ],
     server: {  
         watch: {  
             usePolling: true,  
